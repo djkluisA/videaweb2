521ddcd107f821ed56ced50a368c5e8ed3b0f302
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-07-24 18:23:31 +0200

    creamos el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './compartido/header/header.component';
+import { FooterComponent } from './compartido/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
    
   ],
   imports: [
