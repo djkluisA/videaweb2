37f1eb0ed6d0f443ed471e1d457c392857c1e2df
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-07-24 19:35:01 +0200

    footer comprobar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BodyComponent } from './compartido/body/body.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
     BodyComponent
   ],
   imports: [
