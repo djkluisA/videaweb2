8ee7685db31347259930d64422819558c1693a0d
djkluisA <djkluis@gmail.com>
2019-07-24 19:15:09 +0200

    libreria cdn bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './compartido/header/header.component';
+import { BodyComponent } from './compartido/body/body.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
-   
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
