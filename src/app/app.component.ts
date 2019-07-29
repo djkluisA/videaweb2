942f56b2fac2ad8d35705024e0dc7f8fd5e3bdb9
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-07-29 22:41:26 +0200

    enrutado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'videaweb2';
+  title = 'Tus soluciones';
+
+  mostrar = false;
+
 }
