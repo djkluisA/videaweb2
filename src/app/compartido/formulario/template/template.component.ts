69501222d9a8ffb2a08943407c78498262970960
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-08-01 02:30:44 +0200

    recuperando formulario

---
+++ src/app/compartido/formulario/template/template.component.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent {
+
+  usuario: Object = {
+    servicio: "",
+    nombre: null,
+    telefono: null,
+    email: null
+  }
+
+  constructor() { }
+
+
+  guardar( forma: any ) {
+    console.log('Formulario posteado');
+    console.log( "ngForm", forma );
+    console.log( "valor", forma.value );
+  }
+
+}
